d914376aa513ce67a5f01135151a343076010328
Nikolai Warner <n@nikolaiwarner.com>
2009-12-07 00:29:03 -0500

    updating mailer settings

--- app/models/application_mailer.rb
+++ app/models/application_mailer.rb
@@ -3,7 +3,7 @@ class ApplicationMailer < ActionMailer::Base
 
   def send_weekly_newsletter(options)
     @subject      = 'Collexion Newsletter - ' + Date.today.to_s(:db)
-    @recipients   = 'nickwarner@gmail.com' # collexion@googlegroups.com  
+    @recipients   = 'collexion@googlegroups.com'
     @from         = 'nick@nwarner.com'
     @sent_on      = Time.now
     @content_type = 'text/html'
