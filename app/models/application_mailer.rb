06d6d07c84164b2e6c74f2e8b08bd58ef1bf45ae
nikolaiwarner <n@nikolaiwarner.com>
2010-01-03 12:32:59 -0500

    added in Shemail(mail_styles) plugin for better html email styling

--- app/models/application_mailer.rb
+++ app/models/application_mailer.rb
@@ -1,13 +1,18 @@
 class ApplicationMailer < ActionMailer::Base
   
+  ActionMailer::Base.default_url_options[:host] = "canada.nikolaiwarner.com"
+  
+  
 
-  def send_weekly_newsletter(options)
-    @subject      = 'Collexion Newsletter - ' + Date.today.to_s(:db)
-    @recipients   = 'nickwarner@gmail.com' #'collexion@googlegroups.com'
-    @from         = 'nick@nwarner.com'
-    @sent_on      = Time.now
-    @content_type = 'text/html'
-    @body[:body]  = options[:body]
+  def send_weekly_newsletter(content)
+    css :email
+
+    subject       'Collexion Newsletter - ' + Date.today.to_s(:db)
+    recipients    'nickwarner@gmail.com' #'collexion@googlegroups.com'
+    from          'nick@nwarner.com'
+    sent_on       Time.now
+    content_type  'text/html'
+    body          :body => content
   end
 
 end
